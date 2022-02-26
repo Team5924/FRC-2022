8ed6d3e0986d587fd6110bad58d622b42f814ea1
cyclo9 <32120227+Cyclo9@users.noreply.github.com>
2022-02-26 12:45:36 -0800

    Changed PS4Controller to XboxController

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.TankDrive;
@@ -23,7 +23,7 @@ public class RobotContainer {
   public static final DriveSubsystem m_drivetrain = new DriveSubsystem();
   public static final LimelightSubsystem m_limelight = new LimelightSubsystem();
 
-  PS4Controller m_driverController = new PS4Controller(OIConstants.DRIVER_CONTROLLER_PORT);
+ XboxController m_driverController = new XboxController(OIConstants.DRIVER_CONTROLLER_PORT);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
