094d1cde3c5c22c9a6885196b3de2dc58550e5fa
Chris Voon <christopher.voon@gmail.com>
2022-02-27 18:47:24 -0800

    Change standard of naming objects for intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -13,33 +13,33 @@ import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-    private final Compressor compressor = new Compressor(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM);
+    private final Compressor m_compressor = new Compressor(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM);
 
-    private final DoubleSolenoid leftDoubleSolenoid = new DoubleSolenoid(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM, IntakeConstants.LEFT_PNEUMATIC_FORWARD, IntakeConstants.LEFT_PNEUMATIC_REVERSE);
-    private final DoubleSolenoid rightDoubleSolenoid = new DoubleSolenoid(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM, IntakeConstants.RIGHT_PNEUMATIC_FORWARD, IntakeConstants.RIGHT_PNEUMATIC_REVERSE);
+    private final DoubleSolenoid m_leftDoubleSolenoid = new DoubleSolenoid(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM, IntakeConstants.LEFT_PNEUMATIC_FORWARD, IntakeConstants.LEFT_PNEUMATIC_REVERSE);
+    private final DoubleSolenoid m_rightDoubleSolenoid = new DoubleSolenoid(IntakeConstants.CTRE_PCM, PneumaticsModuleType.CTREPCM, IntakeConstants.RIGHT_PNEUMATIC_FORWARD, IntakeConstants.RIGHT_PNEUMATIC_REVERSE);
 
-    private final CANSparkMax leaderIntakeSpark = new CANSparkMax(IntakeConstants.LEADER_INTAKE_SPARK, MotorType.kBrushless);
-    private final CANSparkMax followerIntakeSpark = new CANSparkMax(IntakeConstants.FOLLOWER_INTAKE_SPARK, MotorType.kBrushless);
+    private final CANSparkMax m_leaderIntakeSpark = new CANSparkMax(IntakeConstants.LEADER_INTAKE_SPARK, MotorType.kBrushless);
+    private final CANSparkMax m_followerIntakeSpark = new CANSparkMax(IntakeConstants.FOLLOWER_INTAKE_SPARK, MotorType.kBrushless);
 
     public IntakeSubsystem() {
-        compressor.disable();
+        m_compressor.disable();
 
-        leftDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
-        rightDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+        m_leftDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+        m_rightDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
 
-        leaderIntakeSpark.restoreFactoryDefaults();
-        followerIntakeSpark.restoreFactoryDefaults();
+        m_leaderIntakeSpark.restoreFactoryDefaults();
+        m_followerIntakeSpark.restoreFactoryDefaults();
 
         // followerIntakeSpark is inverted relative to leaderIntakeSpark
-        followerIntakeSpark.follow(leaderIntakeSpark, true);
+        m_followerIntakeSpark.follow(m_leaderIntakeSpark, true);
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putString("Left Pneumatic State", solenoidStatusAsString(leftDoubleSolenoid));
-        SmartDashboard.putString("Right Pneumatic State", solenoidStatusAsString(rightDoubleSolenoid));
-        SmartDashboard.putBoolean("Compressor Status", compressor.enabled());
-        SmartDashboard.putBoolean("Enough Pressure", compressor.getPressureSwitchValue());
+        SmartDashboard.putString("Left Pneumatic State", solenoidStatusAsString(m_leftDoubleSolenoid));
+        SmartDashboard.putString("Right Pneumatic State", solenoidStatusAsString(m_rightDoubleSolenoid));
+        SmartDashboard.putBoolean("Compressor Status", m_compressor.enabled());
+        SmartDashboard.putBoolean("Enough Pressure", m_compressor.getPressureSwitchValue());
         SmartDashboard.putBoolean("Intake Deployed", isIntakeDeployed());
     }
 
@@ -57,46 +57,46 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public void deployIntake() {
-        leftDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
-        rightDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
+        m_leftDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
+        m_rightDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
     }
 
     public void retractIntake() {
-        leftDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
-        rightDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+        m_leftDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+        m_rightDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
     }
 
     public boolean isIntakeDeployed() {
         // returns the status of the intake, used to determine whether to retract or deploy intake
-        return leftDoubleSolenoid.get().equals(DoubleSolenoid.Value.kForward) && rightDoubleSolenoid.get().equals(DoubleSolenoid.Value.kForward); 
+        return m_leftDoubleSolenoid.get().equals(DoubleSolenoid.Value.kForward) && m_rightDoubleSolenoid.get().equals(DoubleSolenoid.Value.kForward); 
     }
 
     public void enableCompressor() {
-        compressor.enableDigital();
+        m_compressor.enableDigital();
     }
 
     public void disableCompressor() {
-        compressor.disable();
+        m_compressor.disable();
     }
 
     public boolean isCompressorOn() {
-        return compressor.enabled();
+        return m_compressor.enabled();
     }
 
     public void enableIntakeMotor() {
-        leaderIntakeSpark.set(0.3);
+        m_leaderIntakeSpark.set(0.3);
     }
 
     public void disableIntakeMotor() {
-        leaderIntakeSpark.stopMotor();
+        m_leaderIntakeSpark.stopMotor();
     }
 
     public void reverseMotor() {
-        leaderIntakeSpark.set(-0.3);
+        m_leaderIntakeSpark.set(-0.3);
     }
 
     public boolean isIntakeMotorRunning() {
         // intakeSpark.get() returns 0 when the motor is not running
-        return leaderIntakeSpark.get() != 0;
+        return m_leaderIntakeSpark.get() != 0;
     }
 }
