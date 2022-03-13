ba9355f974424fd7433c4948a122242cecb5e858
cyclo9 <32120227+Cyclo9@users.noreply.github.com>
2022-03-13 01:34:41 -0800

    Updated RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import frc.robot.commands.ToggleCompressor;
 import frc.robot.commands.ToggleIntakeDeployed;
 import frc.robot.commands.ToggleIntakeMotor;
 import frc.robot.commands.auto.SingleBallAuto;
+import frc.robot.commands.auto.DoubleBallAuto;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.HorizontalConveyorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -30,9 +31,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
@@ -54,11 +58,16 @@ public class RobotContainer {
   JoystickButton rightBumper = new JoystickButton(m_driverController, XboxController.Button.kRightBumper.value);
 
   // Declaring sendableObject for Autonomous here
-  private final Command m_singleAuto = new SingleBallAuto(m_limelight, m_shooter, m_turret, m_verticalConveyor, m_drivetrain);
-  
+  private final Command m_singleAuto = new SingleBallAuto(m_limelight, m_shooter, m_turret, m_verticalConveyor,
+      m_drivetrain);
+  private final Command m_doubleAuto = new DoubleBallAuto(m_limelight, m_shooter, m_turret, m_verticalConveyor,
+      m_drivetrain);
+
   SendableChooser<Command> m_autoChooser = new SendableChooser<>();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     m_intake.register();
     m_horizontalConveyor.register();
@@ -66,22 +75,29 @@ public class RobotContainer {
     m_shooter.register();
     m_turret.register();
 
-    //m_drivetrain.setDefaultCommand(new TankDrive(m_drivetrain, m_driverController::getLeftY, m_driverController::getRightY));
-    //m_horizontalConveyor.setDefaultCommand(new RunHorizontalConveyor(m_horizontalConveyor, m_verticalConveyor, m_intake));
-    //m_verticalConveyor.setDefaultCommand(new RunVerticalConveyor(m_verticalConveyor, m_intake));
-    //m_shooter.setDefaultCommand(new MaintainShooterSpeed(m_shooter));
+    // m_drivetrain.setDefaultCommand(new TankDrive(m_drivetrain,
+    // m_driverController::getLeftY, m_driverController::getRightY));
+    // m_horizontalConveyor.setDefaultCommand(new
+    // RunHorizontalConveyor(m_horizontalConveyor, m_verticalConveyor, m_intake));
+    // m_verticalConveyor.setDefaultCommand(new
+    // RunVerticalConveyor(m_verticalConveyor, m_intake));
+    // m_shooter.setDefaultCommand(new MaintainShooterSpeed(m_shooter));
 
     // Configure the button bindings
     configureButtonBindings();
-    
+
     m_autoChooser.setDefaultOption("Single Ball Auto", m_singleAuto);
+    m_autoChooser.addOption("Double Ball Auto", m_doubleAuto);
+
     SmartDashboard.putData(m_autoChooser);
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
@@ -90,7 +106,7 @@ public class RobotContainer {
     x.whenPressed(new ToggleIntakeMotor(m_intake));
     y.whenHeld(new ReverseIntakeMotor(m_intake));
 
-    //rightBumper.whenHeld(new Shoot(m_verticalConveyor, m_limelight, m_shooter));
+    // rightBumper.whenHeld(new Shoot(m_verticalConveyor, m_limelight, m_shooter));
   }
 
   /**
