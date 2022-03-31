922dea51c0ffc193b07a8041a32362ba737daa8d
Chris Voon <christopher.voon@gmail.com>
2022-03-31 10:04:39 -0700

    Remove encoder for climber

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -5,17 +5,14 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 
 public class ClimberSubsystem extends SubsystemBase {
   CANSparkMax m_leaderClimberSpark = new CANSparkMax(ClimberConstants.LEADER_CLIMBER_SPARK, MotorType.kBrushed);
   CANSparkMax m_followerClimberSpark = new CANSparkMax(ClimberConstants.FOLLOWER_CLIMBER_SPARK, MotorType.kBrushed);
-  RelativeEncoder m_leaderClimberEncoder = m_leaderClimberSpark.getEncoder();
 
   /** Creates a new ClimberSubsystem. */
   public ClimberSubsystem() {
@@ -25,7 +22,6 @@ public class ClimberSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Climber Position", m_leaderClimberEncoder.getPosition());
   }
 
   public void extend() {
